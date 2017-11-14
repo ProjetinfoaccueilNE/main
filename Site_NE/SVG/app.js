a0d053175f42a3165e683088a14a67b29d495a3e
MaelGalliot <mael.galliot@gmail.com>
2017-11-14 18:37:30 +0100

    Carte interactive V0.1

--- Site_NE/SVG/app.js
+++ Site_NE/SVG/app.js
@@ -8,14 +8,17 @@ var links = document.querySelector('#svg_liste')
 var activeArea = function (id,posX,posY) {
 	links.querySelectorAll('.active').forEach(function(item) {
 		item.classList.remove('active')
+		item.classList.add('hidden')
+
 	})
 	if(id !== undefined)
 	{
+		links.querySelector('#list_'+ id).classList.remove('hidden')
 		links.querySelector('#list_'+ id).classList.add('active')
 		links.querySelector('#list_'+ id).style.position = 'absolute'	
 		console.log(posX,posY)	
-		links.querySelector('#list_'+ id).style.top = posX+10+"px"
-		links.querySelector('#list_'+ id).style.right = posY+10+"px"
+		links.querySelector('#list_'+ id).style.top = posY+(-120)+"px"
+		links.querySelector('#list_'+ id).style.left = posX+(-50)+"px"
 	}
 }
 
@@ -23,8 +26,9 @@ circle.forEach( function(path) {
 	path.addEventListener('mouseenter', function (e)
 	{
 		var id = this.id.replace('circle_','')
-		var posX = e.offsetX;
-		var posY = e.offsetY;
+
+		var posX = e.clientX;
+		var posY = e.clientY;
 		console.log(posX,posY)
 		activeArea(id,posX,posY)
 	})
