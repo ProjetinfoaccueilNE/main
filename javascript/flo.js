847ca226c1e80a00f4275075eb7d6c861ad5e27a
Geoffroy Floriane <fgeoffroy@github.com>
2018-01-03 15:48:15 +0100

    .

--- javascript/flo.js
+++ javascript/flo.js
@@ -2,8 +2,8 @@
 console.log("j'arrive dans le game");
 var prenom = prompt("Entrez votre prénom :");
 console.log("Bonjour, " + prenom);
-//var nb = Number(prompt("Entrez un nombre : ")); // nb est de type nombre et prompt renvoie une char
-//var message="SEND NUDES"
-//for (var i = 0; i <11 ; i++) {
-//	console.log(message[:i])
-//}
+var nb = Number(prompt("Entrez un nombre : ")); // nb est de type nombre et prompt renvoie une char
+var message="SEND NUDES"
+for (var i = 0; i < message.length ; i++) {
+	console.log(message[:i]);
+}
