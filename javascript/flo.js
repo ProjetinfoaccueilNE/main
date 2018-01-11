551e84903a6245621189e6dc2247c37c523bb4b4
lkilipoutte <laure97.feuillet@orange.fr>
2018-01-11 10:46:06 +0100

    .

--- javascript/flo.js
+++ javascript/flo.js
@@ -5,5 +5,14 @@ console.log("Bonjour, " + prenom)
 var nb = Number(prompt("Entrez un nombre : ")) // nb est de type nombre et prompt renvoie une char
 var message="SEND NUDES"
 for (var i = 0; i < message.length ; i++) {
+<<<<<<< HEAD
+	if(i==0)
+		var msg = message[i];
+	else 
+		msg = msg +  message[i];
+	console.log(msg)
+}
+=======
 	console.log(message[i])
 }
+>>>>>>> cbcd708a7a9235eea74e204231e21707e7bd95e5
