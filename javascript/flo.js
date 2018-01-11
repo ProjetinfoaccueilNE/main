c577333f63a90a7e600fdcd63b19b326b00a3f96
MaelGalliot <mael.galliot@gmail.com>
2018-01-11 10:36:48 +0100

    js flo

--- javascript/flo.js
+++ javascript/flo.js
@@ -5,5 +5,9 @@ console.log("Bonjour, " + prenom);
 var nb = Number(prompt("Entrez un nombre : ")); // nb est de type nombre et prompt renvoie une char
 var message="SEND NUDES"
 for (var i = 0; i < message.length ; i++) {
-	console.log(message[:i]);
+	if(i==0)
+		var msg = message[i];
+	else 
+		msg = msg +  message[i];
+	console.log(msg)
 }
