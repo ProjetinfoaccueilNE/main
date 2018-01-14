95ac67978d6480e5b47f09c1c6780c9dd8509f5d
lkilipoutte <laure97.feuillet@orange.fr>
2018-01-14 13:57:31 +0100

    slide

--- javascript/laure.js
+++
@@ -1,4 +0,0 @@
-// Gestion de l'appui sur une touche du clavier produisant un caractère
-document.addEventListener("keypress", function (e) {
-    console.log("Vous avez appuyé sur la touche " + String.fromCharCode(e.charCode));
-});
